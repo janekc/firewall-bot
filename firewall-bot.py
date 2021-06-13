5fa83c9327b5c9a145cba23d8a3454191e85bd30
truttruthahnhahn <elina.katsendorn@mailbox.org>
2021-06-13 10:02:54 -0700

    System overview rewritten into a more beautiful output

--- firewall-bot.py
+++ firewall-bot.py
@@ -5,7 +5,6 @@ from deltachat import Chat, Contact, Message
 import socket
 import pyufw as ufw
 import segno
-import nmap3
 import json
 
 version = "0.7"
@@ -28,9 +27,12 @@ def deltabot_init(bot):
     bot.commands.register(name="/getrules", func=cmd_getrules)
     bot.commands.register(name="/showlistening", func=cmd_showlistening)
     bot.commands.register(name="/setlogging", func=cmd_setlogging)
-
     bot.commands.register(name="/guided", func=cmd_guided)
     bot.commands.register(name="/overview", func=cmd_sysOverView)
+    bot.commands.register(name="/open", func=cmd_sysopen)
+    bot.commands.register(name="/next", func=cmd_sysopen)
+    bot.commands.register(name="/create", func=cmd_syscreate)
+    bot.commands.register(name="/skip", func=cmd_syskip)
     bot.commands.register(name="/allow", func=cmd_allowOrdeny)
     bot.commands.register(name="/deny", func=cmd_allowOrdeny)
     bot.commands.register(name="/out", func=cmd_InOutOrSkip)
@@ -92,6 +94,10 @@ def cmd_disable(command, replies):
         replies.add("Firewall disabled!")
 
 
+# ======== Kommentar zu 'cmd_reset': Löscht zwar alle existierenden Regeln jedoch auch BEFORE Rules in /etc/ufw die zum aussperren führen könnten ====
+# ======== Außerdem wird nicht dafür gesorgt, dass SSH und mDNS erlaubt wird. --> RESET löschen? :) ? ===============================
+
+
 def cmd_reset(command, replies):
     """
     Returns the firewall to it's install defaults. incoming=deny, outgoing=allow, routed=reject
@@ -131,6 +137,7 @@ def cmd_status(command, replies):
             replies.add("status: {}".format(status["status"]))
 
 
+# =========== Man kann sich mit 'cmd_setdefault' aussperren ============
 def cmd_setdefault(command, replies):
     """
     Set the default policies for incoming, outgoing and routed. Policies to choose from are allow, deny and reject.
@@ -166,8 +173,9 @@ def cmd_add(command, replies):
     Returns the raw iptables rule added (incase your interested)
     """
     if check_priv(dbot, command.message):
+        # var 'rule' wird nicht verwendet
         rule = ufw.add(command.payload)
-        dbot.logger.info("\n\Added Rule:\n{}\n".format(command.payload))
+        dbot.logger.info("\nAdded Rule:\n{}\n".format(command.payload))
         replies.add(
             "The following iptables rule was added:\n{}".format(command.payload)
         )
@@ -207,7 +215,7 @@ def cmd_showlistening(command, replies):
 
 def cmd_setlogging(command, replies):
     """
-    Set the ufw logging level. Choose from: 'on', 'off', 'low', 'medium', 'high', 'full'. Check out man ufw for more info on logging.
+    Set the ufw logging level. Choose from: 'on', 'off', 'low', 'medium', 'high', 'full'. Check out man ufw for more info on logging.      ===============================
     """
     if check_priv(dbot, command.message):
         if command.payload in ["on", "off", "low", "medium", "high", "full"]:
@@ -223,31 +231,117 @@ def cmd_setlogging(command, replies):
 # ======== Guided Mode Commands ===============
 
 UFW_CMD = {"cmd1": "", "cmd2": "", "cmd3": "", "cmd4": "", "cmd5": ""}
-nmap = nmap3.NmapHostDiscovery()
+SYS_UFW = ""
+System = {"Protocol": "", "Address": "", "Port": "", "Service": "", "ExistingRule": ""}
+
 
 def cmd_guided(command, replies):
     """
-    Guided use of the firewall-chatbot
+    Guided use of the firewall-chatbot. With the command /guided you can start the Guided Mode.
     """
     if check_priv(dbot, command.message):
         dbot.logger.info("\nStarted the Guided Mode!\n")
         replies.add(
-            "Started Guided Mode. What do you want to do with the Firewall?\n\n Pleas type /allow /deny or /reject"
+            "Started Guided Mode. What do you want to do with the Firewall?\n\nIf you first want to have an overview of your system and its services write /overview. \n\nBut if you want to create a new rule directly \n\n please type /allow /deny or /reject instead."
         )
+
+
 def cmd_sysOverView(command, replies):
     """
-    Guided use of the firewall-chatbot
+    With this command the bot will show you listening ports, applications and rules that apply on this system.
     """
     if check_priv(dbot, command.message):
-        dbot.logger.info("\nStarted the NMAP Command\n")
-        nmapscan = nmap.nmap_portscan_only("localhost", args="-sV")
-        results = json.dumps(nmapscan, indent=4, sort_keys=True)
-        dbot.logger.info("NMAP-Result: {}".format(str(results)))
-        replies.add("NMAP-Result: {}".format(str(results)))
+
+        dbot.logger.info("\nStarted Systemscan\n")
+        sysover = ufw.show_listening()
+        next = 1
+        for sys in sysover:
+            System["Protocol"] = sys[0]
+            if sys[1] == "*":
+                System["Address"] = "all"
+            else:
+                System["Address"] = sys[1]
+            System["Port"] = sys[2]
+            System["Service"] = sys[3]
+            if not sys[-1]:
+                System["ExistingRule"] = "No"
+            else:
+                System["ExistingRule"] = sys[-1]
+            sysoverSorted = json.dumps(System, indent=2, sort_keys=False)
+            dbot.logger.info("Your System: {} \n\n {}".format(sysover, type(sysover)))
+            replies.add(
+                "{}. System: {}\n\n if you want to open something from your system type /open\n".format(
+                    next, sysoverSorted
+                )
+            )
+            next += 1
+
+
+def cmd_sysopen(command, replies):
+    """
+    With this command you will be asked if you want to create an allow rule to open the first listening port that does not currently have a rule in your firewall.
+    """
+    global SYS_UFW
+
+    if check_priv(dbot, command.message):
+        sysover = ufw.show_listening()
+        for sys in sysover:
+            rule = sys[-1]
+            if not rule:
+                SYS_UFW = str(sys[2])
+                replies.add(
+                    "Do you want open port {} to be reachable for service {}? Type /create to create this new rule or /skip if you want to do something specific.".format(
+                        str(sys[2]), str(sys[3])
+                    )
+                )
+                break
+
+
+def cmd_syscreate(command, replies):
+    """
+    With this command you say that you want to create a rule for this listening port.
+    """
+    global SYS_UFW
+    if check_priv(dbot, command.message):
+        rule = "allow " + SYS_UFW
+        ufw.add(rule)
+        replies.add(
+            f"\n\nCreated new Rule: {rule}! to check the next listening port type /next"
+        )
+
+
+def cmd_syskip(command, replies):
+    """
+    This command skips the currently listening port, and the bot then asks you to the next listening port if you want to set up a rule for that port.
+    """
+    global SYS_UFW
+    if check_priv(dbot, command.message):
+        sysover = ufw.show_listening()
+        for index, sys in enumerate(sysover):
+            next = index + 1
+            if next < len(sysover):
+                nxtsys = sysover[next]
+                rule = nxtsys[-1]
+                if not rule:
+                    SYS_UFW = str(sys[2])
+                    replies.add(
+                        "\n\nSkipped Port {}!\n\n Do you want open port {} to be reachable for service {}? Type /create to create this new rule or /skip if you want to do something specific.".format(
+                            SYS_UFW, str(nxtsys[2]), str(nxtsys[3])
+                        )
+                    )
+                    SYS_UFW = str(nxtsys[2])
+                    break
+            else:
+                replies.add(
+                    "\n\nSkipped Port {}!\n\n You have no more listening ports on your system to open. If you want to create another rule then write either /allow /deny or /reject to continue with the Guided Mode.".format(
+                        SYS_UFW
+                    )
+                )
+
 
 def cmd_allowOrdeny(command, replies):
     """
-    Guided use of the firewall-chatbot
+    With the /allow, /deny or /reject command you can tell the bot what you want to set up for a new rule. The difference between reject and deny is that with reject the sender of the network packet receives a message that it has been rejected.
     """
     global UFW_CMD
 
@@ -262,15 +356,18 @@ def cmd_allowOrdeny(command, replies):
 
 def cmd_InOutOrSkip(command, replies):
     """
-    Guided use of the firewall-chatbot
+    With /in, /out or /- is defined which kind of traffic it is. outgoing (/out) or incoming (/in). With the command /- (stands for skip) you can skip this step. By default, incoming traffic is meant.
     """
     global UFW_CMD
 
     if check_priv(dbot, command.message):
-        if command.message.text.strip() is "/in" or "/out":
+        if (
+            command.message.text.strip() == "/in"
+            or command.message.text.strip() == "/out"
+        ):
             UFW_CMD["cmd2"] = command.message.text.strip("/") + " "
             replies.add(
-                "Do you have an specific Ip-address from which you will {}traffic?\n\n Please type the /from IP-Address or \n /-from if you want to skip".format(
+                "Do you have an specific IP-address from which you will {}traffic?\n\n Please type the /from IP-address or \n /-from if you want to skip".format(
                     UFW_CMD["cmd1"]
                 )
             )
@@ -278,7 +375,7 @@ def cmd_InOutOrSkip(command, replies):
         if command.message.text.strip() == "/-":
             UFW_CMD["cmd2"] = ""
             replies.add(
-                "Do you have an specific IP-Address from which you will {}traffic?\n\n Please type the /from IP-Address or \n /-from if you want to skip".format(
+                "Do you have an specific IP-address from which you will {}traffic?\n\n Please type the /from IP-address or \n /-from if you want to skip".format(
                     UFW_CMD["cmd1"]
                 )
             )
@@ -286,14 +383,14 @@ def cmd_InOutOrSkip(command, replies):
 
 def cmd_from(command, replies):
     """
-    Guided use of the firewall-chatbot
+    With /from you can specify an IP-address from which traffic is accepted or denied.
     """
     global UFW_CMD
 
     if check_priv(dbot, command.message):
         UFW_CMD["cmd3"] = "from " + command.payload + " "
         replies.add(
-            "Do you have an specific IP-Address to which you will {}traffic?\n\n Please type the /to IP-Address or /-to if you want to skip".format(
+            "Do you have an specific IP-address to which you will {}traffic?\n\n Please type the /to IP-address or /-to if you want to skip".format(
                 UFW_CMD["cmd1"]
             )
         )
@@ -301,14 +398,14 @@ def cmd_from(command, replies):
 
 def cmd_fromSkip(command, replies):
     """
-    Guided use of the firewall-chatbot
+    With /-from you can skip the 'from IP-address' step.
     """
     global UFW_CMD
 
     if check_priv(dbot, command.message):
         UFW_CMD["cmd3"] = "from any "
         replies.add(
-            "Do you have an specific IP-Address to which you will {}traffic?\n\n Please type the /to IP-Address or /-to if you want to skip".format(
+            "Do you have an specific IP-address to which you will {}traffic?\n\n Please type the /to IP-Address or /-to if you want to skip".format(
                 UFW_CMD["cmd1"]
             )
         )
@@ -316,7 +413,7 @@ def cmd_fromSkip(command, replies):
 
 def cmd_to(command, replies):
     """
-    Guided use of the firewall-chatbot
+    With /to you can specify an IP-address to which traffic is accepted or denied.
     """
     global UFW_CMD
 
@@ -331,7 +428,7 @@ def cmd_to(command, replies):
 
 def cmd_toSkip(command, replies):
     """
-    Guided use of the firewall-chatbot
+    With /-to you can skip the 'to IP-address' step.
     """
     global UFW_CMD
 
@@ -346,7 +443,7 @@ def cmd_toSkip(command, replies):
 
 def cmd_port(command, replies):
     """
-    Guided use of the firewall-chatbot
+    With /port you can specify an port for the new rule.
     """
     global UFW_CMD
 
@@ -355,13 +452,13 @@ def cmd_port(command, replies):
         ufwcommand = command_build()
         dbot.logger.info("Build follow command: {}".format(ufwcommand))
         replies.add(
-            "Is this command '{}' right?\n\n type /yes or /no".format(ufwcommand)
+            "Is this command \n\n '{}' right? \n\n type /yes or /no".format(ufwcommand)
         )
 
 
 def cmd_portSkip(command, replies):
     """
-    Guided use of the firewall-chatbot
+    With /-port you can skip the 'port' step.
     """
     global UFW_CMD
 
@@ -388,8 +485,7 @@ def command_build():
 
 def command_correct(command, replies):
     """
-    Guided use of the firewall-chatbot
-    TODO: Escaping the ufwcommand befor ufw.add()
+    The /yes /no command is used to either accept the created command with /yes in the firewall or to reject it with /no.
     """
     global UFW_CMD
 
